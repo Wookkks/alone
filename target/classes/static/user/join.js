ab5158e4e675634149696547e5370337740b3c26
Wookkks <discdao@gmail.com>
2024-01-05 17:58:32 +0900

    회원가입 ajax 구현중

--- target/classes/static/user/join.js
+++ target/classes/static/user/join.js
@@ -1,50 +1,46 @@
 let join = {
     template: `
-        <div>
-            <h3>component test</h3>
-        </div>
+        <div> component test </div>
     `
     ,
     data() {
         return {
             signup: {
+                name: null,
                 email: null,
                 password: null,
             }
         }
     },
     methods: {
+        join : $(function (){
+            $('#btn-join').click(function (){
+                let data = {
+                    name: $('#name').val(),
+                    email: $('#email').val(),
+                    password: $('#password').val(),
 
+                }
+                console.log("ajax")
+                $.ajax({
+                    type: "POST",
+                    url: "user/join",
+                    data: JSON.stringify(data),
+                    contentType: "application/json; charset=utf-8",
+                    dataType: "json",
+                }).done(function (res){
+                    if(res.status === 500) {
+                        alert('회원가입 실패');
+                    } else {
+                        alert('회원가입 완료')
+                    }
+                    location.href = "/";
+                }).fail(function (err) {
+                    alert(JSON.stringify(err));
+                })
+
+            })
+        })
     }
 }
 
-$(function () {
-
-    $('#btn-join').click(function (){
-        let data = {
-            name: $('#name').val(),
-            email: $('#email').val(),
-            password: $('#password').val(),
-
-        }
-
-        console.log("ajax")
-        $.ajax({
-            type: "POST",
-            url: "user/join",
-            data: JSON.stringify(data),
-            contentType: "application/json; charset=utf-8",
-            dataType: "json",
-        }).done(function (res){
-            if(res.status === 500) {
-                alert('회원가입 실패');
-            } else {
-                alert('회원가입 완료')
-            }
-            location.href = "/";
-        }).fail(function (err) {
-            alert(JSON.stringify(err));
-        })
-
-    })
-})
